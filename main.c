09eb8f87395a8e3c048a1c543a0eddb7c2911c08
Matěj Marčišovský <matej@marcisovsky.cz>
2022-01-07 19:24:37 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,56 +1,63 @@
 #include <OneWire.h>
 
-#define I_BUTTON_PIN 5
-#define BUTTON_PIN 4
+#define BUFFER_SIZE 20
+#define BUTTON_PIN 8
+#define BUZZER_FREQUENCY 4000
+#define BUZZER_PIN 7
+#define PROBE_PIN 6
 
-OneWire ibutton(I_BUTTON_PIN);
+OneWire ibutton(PROBE_PIN);
 
-byte buffer[20]; // Array to store the iButton ID.
-int buttonState = 0;
+byte buttonData[BUFFER_SIZE];
+byte controlButtonData[BUFFER_SIZE];
+int currentMode = LOW;
 bool buttonWritten = false;
 
 void setup() {
   pinMode(BUTTON_PIN, INPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
 
   Serial.begin(9600);
   Serial.println("iButton");
 }
 
 void loop() {
-  buttonState = digitalRead(BUTTON_PIN);
+  int buttonState = digitalRead(BUTTON_PIN);
+  if (buttonState != currentMode) {
+    tone(BUZZER_PIN, BUZZER_FREQUENCY, 300);
+    currentMode = buttonState;
+  }
 
-  if (buttonState == LOW) {
-    if (!ibutton.search(buffer)) {
-      ibutton.reset_search();
-      delay(200);
+  if (currentMode == LOW) { // Read Mode
+      if (readButton(buttonData) && readButton(controlButtonData) && verify()) {
+        Serial.println("Reading...");
+        printID();
+        tone(BUZZER_PIN, BUZZER_FREQUENCY, 150);
+        delay(1500);
+      } else {
+        ibutton.reset_search();
+        delay(200);
 
-      return;
-    } else {
-      Serial.println("Reading...");
+        return;
+      }
+  }
+
+  if (currentMode == HIGH) { // Write Mode
+    if (!ibutton.reset()) {
+      buttonWritten = false;
+    } else if (!buttonWritten) {
+      Serial.println("Writing...");
       printID();
-      delay(1000);
+      writeButton();
+
+      if (readButton(controlButtonData) && verify()) {
+        buttonWritten = true;
+
+        Serial.println("Done!");
+        tone(BUZZER_PIN, BUZZER_FREQUENCY, 150);
+        delay(1000);
+      }
     }
-  }
-
-  if (buttonState == HIGH && !ibutton.reset()) {
-    buttonWritten = false;
-  }
-
-  if (buttonState == HIGH && ibutton.reset() && !buttonWritten) {
-    buttonWritten = true;
-    Serial.println("Writing...");
-    printID();
-
-    ibutton.write(0xD5);
-
-    for (int x = 0; x < 8; x++) {
-      writeByte(buffer[x]);
-      Serial.print('-');
-    }
-
-    Serial.println("");
-    Serial.println("Done!");
-    delay(1000);
   }
 }
 
@@ -58,7 +65,31 @@ void printID() {
   Serial.print("ID: ");
 
   for (int x = 0; x < 8; x++) {
-    Serial.print(buffer[x], HEX); // Print the buffer content in LSB. For MSB: for (int x = 8; x>0; x--).
+    Serial.print(buttonData[x], HEX); // Print the buffer content in LSB. For MSB: for (int x = 8; x > 0; x--).
+  }
+
+  Serial.println("");
+}
+
+bool verify() {
+  return memcmp(buttonData, controlButtonData, sizeof(buttonData)) == 0;
+}
+
+bool readButton(byte* data) {
+  if (ibutton.search(data)) {
+    ibutton.reset_search();
+    return true;
+  }
+
+  return false;
+}
+
+void writeButton() {
+  ibutton.write(0xD5);
+
+  for (int x = 0; x < 8; x++) {
+    writeByte(buttonData[x]);
+    Serial.print('-');
   }
 
   Serial.println("");
@@ -67,13 +98,13 @@ void printID() {
 void writeByte(byte data) {
   for(int x = 0; x < 8; x++) {
     if (data & 1) {
-      digitalWrite(I_BUTTON_PIN, LOW); pinMode(I_BUTTON_PIN, OUTPUT);
+      digitalWrite(PROBE_PIN, LOW); pinMode(PROBE_PIN, OUTPUT);
       delayMicroseconds(60);
-      pinMode(I_BUTTON_PIN, INPUT); digitalWrite(I_BUTTON_PIN, HIGH);
+      pinMode(PROBE_PIN, INPUT); digitalWrite(PROBE_PIN, HIGH);
       delay(10);
     } else {
-      digitalWrite(I_BUTTON_PIN, LOW); pinMode(I_BUTTON_PIN, OUTPUT);
-      pinMode(I_BUTTON_PIN, INPUT); digitalWrite(I_BUTTON_PIN, HIGH);
+      digitalWrite(PROBE_PIN, LOW); pinMode(PROBE_PIN, OUTPUT);
+      pinMode(PROBE_PIN, INPUT); digitalWrite(PROBE_PIN, HIGH);
       delay(10);
     }
 
